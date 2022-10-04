91d656e7800f77c824c7b8bf03bb3d05ae184ff6
alexLDSpedroDEV <115034319+alexLDSpedroDEV@users.noreply.github.com>
2022-10-04 15:08:37 -0300

    Add files via upload

---
+++ protifolio profisional/script.js
@@ -0,0 +1,39 @@
+
+function javafunction() {
+    document.getElementById("javaScript").style.display = "block";
+}
+function cssfunction() {
+    document.getElementById("css").style.display = "block";
+}
+function htmlfunction() {
+    document.getElementById("html").style.display = "block";
+}
+function bootstrapfunction() {
+    document.getElementById("bootstrap").style.display = "block";
+}
+/* função para os textos aparecerem */
+function functionmouseoutjs() {
+    document.getElementById("javaScript").style.display = "none";
+}
+function functionmouseoutcss() {
+    document.getElementById("css").style.display = "none";
+}
+function functionmouseouthtml() {
+    document.getElementById("html").style.display = "none";
+}
+function functionmouseoutb() {
+    document.getElementById("bootstrap").style.display = "none";
+}
+/* função para o texto ficar none */
+
+/* função dos menu */
+function functionmenu() {
+    document.getElementById('conteinenButton').style.display = 'block';
+    document.getElementById('span').style.display = 'none';
+    document.getElementById('buttonMenuHeader').style.width = '0px';
+}
+function functionfechar() {
+    document.getElementById('conteinenButton').style.display = 'none';
+    document.getElementById('span').style.display = 'block';
+    document.getElementById('fechar').style.display = 'none';
+}
